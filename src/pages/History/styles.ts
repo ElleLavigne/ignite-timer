d43be97bde16ece29cc9ea7f242ed8599e8de507
Izaelle Lavigne <izaelleavrillavigne@gmail.com>
2024-09-24 15:42:20 -0300

    feathistory table styling

--- src/pages/History/styles.ts
+++ src/pages/History/styles.ts
@@ -46,7 +46,16 @@ export const HistoryList = styled.div`
   td {
     background-color: ${(props) => props.theme["gray-700"]};
     border-top: 4px solid ${(props) => props.theme["gray-800"]};
-    
+    padding: 1rem;
+    font-size: 0.875rem;
+    line-height: 1.6rem;
+    &:first-child {
+      width: 50%;
+      padding-left: 1.5rem;
+    }
+    &:last-child {
+      padding-right: 1.5rem;
+    }
   }
 `
 
