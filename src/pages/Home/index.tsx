7b2f3309ba5d9e2c03bcf14da94dfd1a0e18141d
Izaelle Lavigne <izaelleavrillavigne@gmail.com>
2024-09-27 16:28:11 -0300

    starting new cycles

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,6 +3,7 @@ import { Play } from "phosphor-react"
 import { useForm } from "react-hook-form"
 import { zodResolver } from "@hookform/resolvers/zod"
 import * as zod from "zod"
+import { useState } from "react"
 
 import {
   CountdownConatiner,
@@ -16,7 +17,7 @@ import {
 
 // _______________________________________
 
-// 
+//
 const newCycleFormValidationSchema = zod.object({
   task: zod.string().min(1, "Informe a tarefa"),
   minutesAmount: zod
@@ -24,14 +25,20 @@ const newCycleFormValidationSchema = zod.object({
     .min(5, "O cilco precisa ser de no mínimo de 5 minutos")
     .max(60, "O cilco precisa ser de no mínimo de 5 minutos"),
 })
-  // interface NewCycleFormData {
-  //   task: string
-  //   minutesAmount: number
-  // }
-  type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
-
+// interface NewCycleFormData {
+//   task: string
+//   minutesAmount: number
+// }
+type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
+interface Cycle {
+  id: string
+  task: string
+  minutesAmount: number
+}
 // Criação  do componente.
 export function Home() {
+  const [cycles, setCycles] = useState<Cycle[]>([])
+  const [activeCycleId, setActiveCycleI] = useState<string | null> (null)
   const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
     resolver: zodResolver(newCycleFormValidationSchema),
     defaultValues: {
@@ -40,14 +47,21 @@ export function Home() {
     },
   })
 
-
   function handleCrateNewCycle(data: NewCycleFormData) {
-    console.log(data)
+    const id = String(new Date().getTime())
+    const newCycle: Cycle = {
+      id,
+      task: data.task, 
+      minutesAmount: data.minutesAmount,
+    }
+    setCycles((state) => [...state, newCycle])
+    setActiveCycleI(id)
     reset()
   }
-
+  const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
   const task = watch("task")
   const isSubmitDisabled = !task
+
   return (
     <HomeContainer>
       {/* Criação fo formulário */}
@@ -59,7 +73,7 @@ export function Home() {
             id="task"
             list="task-suggestions"
             placeholder="Dê um nome para o seu projeto"
-            {...register("task")} 
+            {...register("task")}
           />
           <datalist id="task-suggestions">
             <option value="Projeto 1" />
