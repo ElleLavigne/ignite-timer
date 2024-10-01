1bee82300cd7f119d08156c6114ba7833f0897b1
Izaelle Lavigne <izaelleavrillavigne@gmail.com>
2024-10-01 15:15:28 -0300

    ending cycle

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,7 +4,7 @@ import { useForm } from "react-hook-form"
 import { zodResolver } from "@hookform/resolvers/zod"
 import * as zod from "zod"
 import { useEffect, useState } from "react"
-import { differenceInSeconds } from 'date-fns'
+import { differenceInSeconds } from "date-fns"
 
 import {
   CountdownConatiner,
@@ -24,7 +24,7 @@ const newCycleFormValidationSchema = zod.object({
   task: zod.string().min(1, "Informe a tarefa"),
   minutesAmount: zod
     .number()
-    .min(5, "O cilco precisa ser de no mínimo de 5 minutos")
+    .min(1, "O cilco precisa ser de no mínimo de 5 minutos")
     .max(60, "O cilco precisa ser de no mínimo de 5 minutos"),
 })
 // interface NewCycleFormData {
@@ -39,11 +39,12 @@ interface Cycle {
   minutesAmount: number
   startDate: Date
   interruptedDate?: Date
+  finishedDate?: Date
 }
 // Criação  do componente.
 export function Home() {
   const [cycles, setCycles] = useState<Cycle[]>([])
-  const [activeCycleId, setActiveCycleId] = useState<string | null> (null)
+  const [activeCycleId, setActiveCycleId] = useState<string | null>(null)
   const [amountSecondsPassed, setAmountSecondsPassed] = useState(0)
   const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
     resolver: zodResolver(newCycleFormValidationSchema),
@@ -53,28 +54,45 @@ export function Home() {
     },
   })
   const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
+  const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0
 
   useEffect(() => {
     let interval: number
-    if(activeCycle){
-    interval = setInterval(() => {
-        setAmountSecondsPassed(
-          differenceInSeconds(new Date(), activeCycle.startDate)
+    if (activeCycle) {
+      interval = setInterval(() => {
+        const secondsDifference = differenceInSeconds(
+          new Date(),
+          activeCycle.startDate
         )
+        if (secondsDifference >= totalSeconds) {
+          setCycles(state => 
+            state.map((cycle) => {
+              if (cycle.id === activeCycleId) {
+                return { ...cycle, finishedDate: new Date() }
+              } else {
+                return cycle
+              }
+            })
+          )
+          setAmountSecondsPassed(totalSeconds)
+          clearInterval(interval)
+        } else {
+          setAmountSecondsPassed(secondsDifference)
+        }
       }, 1000)
     }
-    return() => {
+    return () => {
       clearInterval(interval)
     }
-  }, [activeCycle])
+  }, [activeCycle, totalSeconds, activeCycleId])
 
   function handleCrateNewCycle(data: NewCycleFormData) {
     const id = String(new Date().getTime())
     const newCycle: Cycle = {
       id,
-      task: data.task, 
+      task: data.task,
       minutesAmount: data.minutesAmount,
-      startDate: new Date()
+      startDate: new Date(),
     }
     setCycles((state) => [...state, newCycle])
     setActiveCycleId(id)
@@ -82,28 +100,26 @@ export function Home() {
     reset()
   }
 
-  function handleInterruptCycle(){
-    setCycles(
-      cycles.map((cycle) => {
+  function handleInterruptCycle() {
+    setCycles( state =>
+      state.map((cycle) => {
         if (cycle.id === activeCycleId) {
-          return {...cycle, interruptedDate: new Date()}
+          return { ...cycle, interruptedDate: new Date() }
         } else {
           return cycle
         }
       })
     )
     setActiveCycleId(null)
-
-      }
-  const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0
+  }
   const currentSeconds = activeCycle ? totalSeconds - amountSecondsPassed : 0
 
   const minutesAmount = Math.floor(currentSeconds / 60)
   const secondsAmount = currentSeconds % 60
-  const minutes = String(minutesAmount).padStart(2, '0')
+  const minutes = String(minutesAmount).padStart(2, "0")
   const seconds = String(secondsAmount).padStart(2, "0")
   useEffect(() => {
-    if(activeCycle){
+    if (activeCycle) {
       document.title = `${minutes}:${seconds}`
     }
   }, [minutes, seconds, activeCycle])
@@ -135,7 +151,7 @@ export function Home() {
             type="number"
             id="minutesAmount"
             placeholder="00"
-            step={5}
+            step={1}
             max={60}
             disabled={!!activeCycle}
             {...register("minutesAmount", { valueAsNumber: true })}
