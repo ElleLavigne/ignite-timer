5fd303f677fb6aa94da3e5e31b5d60cda585b545
Izaelle Lavigne <izaelleavrillavigne@gmail.com>
2024-10-01 14:10:11 -0300

    making the timer work

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,7 +3,8 @@ import { Play } from "phosphor-react"
 import { useForm } from "react-hook-form"
 import { zodResolver } from "@hookform/resolvers/zod"
 import * as zod from "zod"
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import { differenceInSeconds } from 'date-fns'
 
 import {
   CountdownConatiner,
@@ -34,6 +35,7 @@ interface Cycle {
   id: string
   task: string
   minutesAmount: number
+  startDate: Date
 }
 // Criação  do componente.
 export function Home() {
@@ -47,19 +49,34 @@ export function Home() {
       task: "",
     },
   })
+  const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
 
+  useEffect(() => {
+    let interval: number
+    if(activeCycle){
+    interval = setInterval(() => {
+        setAmountSecondsPassed(
+          differenceInSeconds(new Date(), activeCycle.startDate)
+        )
+      }, 1000)
+    }
+    return() => {
+      clearInterval(interval)
+    }
+  }, [activeCycle])
   function handleCrateNewCycle(data: NewCycleFormData) {
     const id = String(new Date().getTime())
     const newCycle: Cycle = {
       id,
       task: data.task, 
       minutesAmount: data.minutesAmount,
+      startDate: new Date()
     }
     setCycles((state) => [...state, newCycle])
     setActiveCycleI(id)
+    setAmountSecondsPassed(0)
     reset()
   }
-  const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
   const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0
   const currentSeconds = activeCycle ? totalSeconds - amountSecondsPassed : 0
 
@@ -67,7 +84,11 @@ export function Home() {
   const secondsAmount = currentSeconds % 60
   const minutes = String(minutesAmount).padStart(2, '0')
   const seconds = String(secondsAmount).padStart(2, "0")
-
+  useEffect(() => {
+    if(activeCycle){
+      document.title = `${minutes}:${seconds}`
+    }
+  }, [minutes, seconds, activeCycle])
   const task = watch("task")
   const isSubmitDisabled = !task
 
